54188b1705e58201c00664214bbea17014684855
krishnaramachandran1973 <navkkrnair@gmail.com>
2021-06-02 21:54:41 +0530

    Changed code a bit

--- src/main/java/com/cts/HelloWorldSpringDI.java
+++ src/main/java/com/cts/HelloWorldSpringDI.java
@@ -2,15 +2,24 @@ package com.cts;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 
 import com.cts.message.MessageRenderer;
 
 public class HelloWorldSpringDI {
 
 	public static void main(String[] args) {
-		ApplicationContext context = new ClassPathXmlApplicationContext("app-context.xml");
-
+		/*ApplicationContext context = new ClassPathXmlApplicationContext("app-context.xml");
+		
 		MessageRenderer renderer = context.getBean("renderer", MessageRenderer.class);
+		renderer.render();*/
+
+		GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
+		ctx.load("classpath:/app-context.xml");
+		ctx.refresh();
+		MessageRenderer renderer = ctx.getBean("renderer", MessageRenderer.class);
 		renderer.render();
+		ctx.close();
 	}
 }
